f2f76b86feae4213ae7c778eddeb4b49f08fc9e5
Arnaud Soulier <soulier.arnaud@hotmail.fr>
2018-03-15 15:48:54 +0100

    add split_sentences function

--- preprocessing.py
+++ preprocessing.py
@@ -1,35 +1,26 @@
 import csv
 import nltk
 from nltk import word_tokenize
+import treetaggerwrapper
 
 def read_csv(csv_file_name):
     csv_file = open(csv_file_name, "r");
     opinions = csv_file.readlines();
     return opinions;
 
+def to_lower(opinions):
+    data = [];
+
+    for opinion in opinions:
+        data.append(opinion.lower());
+
+    return data;
+
 def tokenize(opinions):
     data = [];
 
     for opinion in opinions:
         data.append(word_tokenize(opinion));
-
-    return data;
-
-def remove_stop_words(opinions):
-    stop_words_file = open("rsc/stopwords.csv", "r");
-    stop_words_list = stop_words_file.readlines();
-    stop_words = [];
-    for element in stop_words_list:
-        stop_words.append(word_tokenize(element)[0]);
-
-    data = [];
-
-    for opinion in opinions:
-        tmp = [];
-        for word in opinion:
-            if word not in stop_words:
-                tmp.append(word);
-        data.append(tmp);
 
     return data;
 
@@ -45,25 +36,50 @@ def lemmatize(opinions):
 
     return data;
 
-# def split_sentences(opinions):
-#     data = [];
-#     for opinion in opinions:
-#         tmp = [];
-#         tmp2 = [];
-#         opinion = word_tokenize(opinion);
-#         for word in opinion:
-#             if word == '.':
-#                 tmp2.append(tmp);
-#                 tmp = [];
-#             else:
-#                 tmp.append(word);
-#
-#         data.append(tmp2);
-#         tmp2 = [];
-#
-#     return data;
+def remove_stop_words(opinions):
+    stop_words_file = open("rsc/stopwords.csv", "r");
+    stop_words_list = stop_words_file.readlines();
+    stop_words = [];
+
+    for element in stop_words_list:
+        stop_words.append(word_tokenize(element)[0]);
+
+    data = [];
+
+    for opinion in opinions:
+        tmp = [];
+
+        for word in opinion:
+            if word not in stop_words:
+                tmp.append(word);
+
+        data.append(tmp);
+
+    return data;
+
+def split_sentences(opinions):
+    data = [];
+
+    for opinion in opinions:
+        tmp = [];
+        tmp2 = [];
+
+        for word in opinion:
+            if word == '.':
+                tmp2.append(tmp);
+                tmp = [];
+            else:
+                tmp.append(word);
+
+        data.append(tmp2);
+        tmp2 = [];
+
+    return data;
 
 opinions = read_csv("data/test.csv");
+print(opinions[0]);
+print("");
+opinions = to_lower(opinions);
 print(opinions[0]);
 print("");
 opinions = tokenize(opinions);
@@ -73,5 +89,11 @@ opinions = lemmatize(opinions);
 print(opinions[0]);
 print("");
 opinions = remove_stop_words(opinions);
+print(opinions[0]);
+print("");
+opinions = split_sentences(opinions);
+# for element in opinions[0]:
+#     print(element);
+#     print("");
 print(opinions[0]);
 print("");
