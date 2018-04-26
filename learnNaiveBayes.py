9cfb00899b179e47561182736a5cd10fd012f5b3
Arnaud Soulier <soulier.arnaud@hotmail.fr>
2018-04-26 10:30:47 +0200

    update

--- learnNaiveBayes.py
+++ learnNaiveBayes.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from nltk import word_tokenize
+from sklearn.neighbors import KNeighborsClassifier
 
 def read_csv(csv_file_name):
     csv_file = open(csv_file_name, "r");
@@ -24,16 +25,27 @@ def write_csv(csv_file_name, data):
 opinions_m = read_csv("data/dataset2.csv");
 class_m = read_csv("data/labels.csv");
 test = read_csv("data/test_data2.csv");
+class_test = read_csv("data/test_labels.csv");
 
 opinions_train, opinions_test, class_train, class_test = train_test_split(opinions_m, class_m, test_size=0.0);
 opinions_test_test, opinions_test_train, class_test_test, class_test_train = train_test_split(test, test, test_size=0.0);
 
 vectorizer = TfidfVectorizer();
 opinions_train = vectorizer.fit_transform(opinions_train);
-class_test_test = vectorizer.transform(class_test_test);
+opinions_test_test = vectorizer.transform(opinions_test_test);
 
-clf = MultinomialNB();
-clf.fit(opinions_train, class_train);
-pred = clf.predict(class_test_test);
-print(pred);
-write_csv("data/test_label.csv", pred);
+neigh = KNeighborsClassifier(n_neighbors=100);
+neigh.fit(opinions_train, class_train);
+
+pred = neigh.predict(opinions_test_test);
+
+# vectorizer = TfidfVectorizer();
+# opinions_train = vectorizer.fit_transform(opinions_train);
+# opinions_test_test = vectorizer.transform(opinions_test_test);
+#
+# clf = MultinomialNB();
+# clf.fit(opinions_train, class_train);
+# pred = clf.predict(opinions_test_test);
+# print(pred);
+# print(metrics.accuracy_score(class_test, pred));
+write_csv("data/res_label.csv", pred);
